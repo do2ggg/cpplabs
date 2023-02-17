df34f4cb396761fc1f6d7f996d2edd3b9a735213
do2ggg <113709757+do2ggg@users.noreply.github.com>
2023-02-17 18:46:16 +0200

    Update lab02-01.cpp

--- lab02-01.cpp
+++ lab02-01.cpp
@@ -2,24 +2,7 @@
 #include <string>
 #include <cmath>
 #include <ctime>
-#define reset          "\033[0m"
-#define black          "\033[30m"
-#define red            "\033[31m"
-#define green          "\033[32m"
-#define yellow         "\033[33m"
-#define blue           "\033[34m"
-#define magenta        "\033[35m"
-#define cyan           "\033[36m"
-#define white          "\033[37m"
-#define brightblack    "\033[90m"
-#define brightred      "\033[91m"
-#define brightgreen    "\033[92m"
-#define brightyellow   "\033[93m"
-#define brightblue     "\033[94m"
-#define brightmagenta  "\033[95m"
 #define brightcyan     "\033[96m"
-#define brightwhite    "\033[97m"
-
 
 using namespace std;
  
@@ -37,7 +20,8 @@ int main()
     y=(3*sin(a+(b/c)))/(tan(c-b)+pow(a,2))+(b*c);
     cout << y << "\n";
     cout << brightcyan
-	 << "(code by alexandrov 🦄)\n"; 
+	 << "(code by alexandrov 🦄)\n";
+    system("pause");
     return 0;
 }
 
