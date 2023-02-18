7bdc8a3100aeac879c1caa15eda8f2e1104f7d07
do2ggg <113709757+do2ggg@users.noreply.github.com>
2023-02-18 10:48:47 +0200

    Update lab01-01.cpp

--- lab01-01.cpp
+++ lab01-01.cpp
@@ -2,8 +2,23 @@
 #include <string>
 #include <cmath>
 #include <ctime>
+#define reset          "\033[0m"
+#define black          "\033[30m"
+#define red            "\033[31m"
+#define green          "\033[32m"
+#define yellow         "\033[33m"
+#define blue           "\033[34m"
+#define magenta        "\033[35m"
+#define cyan           "\033[36m"
+#define white          "\033[37m"
+#define brightblack    "\033[90m"
+#define brightred      "\033[91m"
+#define brightgreen    "\033[92m"
+#define brightyellow   "\033[93m"
+#define brightblue     "\033[94m"
+#define brightmagenta  "\033[95m"
 #define brightcyan     "\033[96m"
-
+#define brightwhite    "\033[97m"
 
 using namespace std;
  
@@ -22,7 +37,6 @@ int main()
     cout << y << "\n";
     cout << brightcyan
 	 << "(code by alexandrov 🦄)\n";
-    system("pause");
     return 0;
 }
 
